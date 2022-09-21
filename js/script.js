20d8641ba4a6f0faecc3ba306e69c30ad9ecb5a0
AmanRaj08 <amanraj08072001@gmail.com>
2022-09-22 01:52:13 +0530

    car

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+let sen=document.querySelector('send');
+sen.onclick = () =>{
+  alert("Thank You for registering with us we will contact you for further deatails!!")
+}
